07f77a0f116779943ace1848a56afd849201eeb4
federico Lubbert <federico.lubbert@gmail.com>
2024-04-06 12:45:23 +0200

    removed some images, made order of appearance random

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+import os
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def gallery():
+    image_folder = os.path.join('static', 'images')
+    image_files = [file for file in os.listdir(image_folder) if file.endswith(('png', 'jpg', 'jpeg', 'gif'))]
+    images = ['images/' + file for file in image_files]
+    random.shuffle(images)
+    return render_template('index.html', images=images)
 
 if __name__ == '__main__':
     app.run(debug=True)
